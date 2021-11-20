183e25bafb15184f99ae6df064ce64671189eb09
Vegeta Luu <vegeta.luu@tpfgroup.cc>
2021-11-20 16:57:39 +0700

    backup

--- wp-content/themes/flatsome-child/sources/src/js/main.js
+++ wp-content/themes/flatsome-child/sources/src/js/main.js
@@ -4,4 +4,11 @@ jQuery(document).ready(function(){
     jQuery('.product-small > .col-inner').matchHeight();
     jQuery('.title-wrapper').matchHeight();
     jQuery('.post-item .box-text').matchHeight();
+    if(jQuery('.product-small.product-title').length > 0){
+        jQuery('.product-small.product-title').matchHeight();
+    }
+    if(jQuery('.product-small .price-wrapper').length > 0){
+        jQuery('.product-small .price-wrapper').matchHeight();
+    }
+    
 })
