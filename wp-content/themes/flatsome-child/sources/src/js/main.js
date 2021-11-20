1edf304c3894b5963d2fdadc69bb2931b2807681
Vegeta Luu <vegeta.luu@tpfgroup.cc>
2021-11-20 14:44:11 +0700

    backup

--- wp-content/themes/flatsome-child/sources/src/js/main.js
+++ wp-content/themes/flatsome-child/sources/src/js/main.js
@@ -2,4 +2,6 @@ import '../sass/main.scss';
 import 'jquery-match-height';
 jQuery(document).ready(function(){
     jQuery('.product-small > .col-inner').matchHeight();
+    jQuery('.title-wrapper').matchHeight();
+    jQuery('.post-item .box-text').matchHeight();
 })
